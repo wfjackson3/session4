3af49daafcbbc539c92f7d85677f9c9f907f6bfd
Willis F. Jackson III <raidercat@gmail.com>
2013-04-30 15:39:53 -0500

    firstgit add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Session4::Application.config.secret_token = '9830a5fb4bde8ae152117f58ee49477589db6bb490280fe8b4b01891be1cdb3e79092cabea7db1ea4a860431ff525f6fd51cbb06a59583dd2a881fa2c0c9ab74'
