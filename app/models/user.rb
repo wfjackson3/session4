220af5a988496b35e46b493c0303b44a8586d54d
Willis F. Jackson III <raidercat@gmail.com>
2013-05-12 18:18:44 -0500

    added new user attributes to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
