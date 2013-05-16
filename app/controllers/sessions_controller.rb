8dda28aa22dd51186429298aeffe7de6271634af
Willis F. Jackson III <raidercat@gmail.com>
2013-05-16 15:59:37 -0500

    a bad logical test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by_email(params[:session][:email].downcase)
-	  if # some logic
+	  if user.authenticate(params[:session][:password])
 	    # They are who they say they are
 	  else
 	    # They got something wrong
