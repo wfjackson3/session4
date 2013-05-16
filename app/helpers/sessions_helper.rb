1d36e198d6de05081e14b39cb966a7ec653be847
Willis F. Jackson III <raidercat@gmail.com>
2013-05-16 17:00:56 -0500

    magic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,14 @@
 module SessionsHelper
 	def sign_in(user)
 		session[:remember_token] = user.id
-		@current_user = User.find(session[:remember_token])
+		self.current_user = User.find(session[:remember_token])
 	end
 
 	def sign_out
 		
 	end
+
+	def current_user=(user)
+    @current_user = user
+  end
 end
