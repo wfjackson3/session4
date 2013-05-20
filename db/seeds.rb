15fea7432119c4a1137491c53901ba3dcd691429
Willis F. Jackson III <raidercat@gmail.com>
2013-05-20 13:07:52 -0500

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+	name: "A. Adams",
+	email: "a@example.com",
+	password: "123123",
+	password_confirmation: "123123"
+)
+
+User.create(
+	name: "B. Barker",
+	email: "b@example.com",
+	password: "123123",
+	password_confirmation: "123123"
+)
+
+Event.create(
+	user_id: 1,
+	starts_at: DateTime.now.utc,
+	ends_at: DateTime.now.utc + 1.hour,
+	street_address: "5500 Antioch Road",
+	city: "Mission",
+	state: "KS"
+)
+
+Event.create(
+	user_id: 1,
+	starts_at: DateTime.now.utc + 1.day,
+	ends_at: DateTime.now.utc + 1.day + 1.hour,
+	street_address: "729 North 14th Street",
+	city: "Omaha",
+	state: "NE"
+)
+
+Event.create(
+	user_id: 1,
+	starts_at: DateTime.now.utc + 3.days,
+	ends_at: DateTime.now.utc + 3.days + 1.hour,
+	street_address: "10610 Shawnee Mission Parkway",
+	city: "Shawnee",
+	state: "KS"
+)
+
+Event.create(
+	user_id: 2,
+	starts_at: DateTime.now.utc + 1.week,
+	ends_at: DateTime.now.utc + 1.week + 1.hour,
+	street_address: "5500 Antioch",
+	city: "Merriam",
+	state: "KS"
+)
+
+Event.create(
+	user_id: 2,
+	starts_at: DateTime.now.utc + 8.days,
+	ends_at: DateTime.now.utc + 8.days + 1.hour,
+	street_address: "10703 Shawnee Mission Parkway",
+	city: "Shawnee",
+	state: "KS"
+)
