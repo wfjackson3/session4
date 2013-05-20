dca7765558c63c63636198c5e282982189c0806e
Willis F. Jackson III <raidercat@gmail.com>
2013-05-20 14:13:18 -0500

    changed the event create action to also scope to the current user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -42,7 +42,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(params[:event])
+    @event = current_user.events.new(params[:event])
 
     respond_to do |format|
       if @event.save
