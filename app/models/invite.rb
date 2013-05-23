a11c1bde12becaabf08623c746b3646d6685bf18
Willis F. Jackson III <raidercat@gmail.com>
2013-05-23 13:18:13 -0500

    annotated new models

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: invites
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  invitee_name    :string(255)
+#  suggested_start :datetime
+#  suggested_end   :datetime
+#  street_address  :string(255)
+#  city            :string(255)
+#  state           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Invite < ActiveRecord::Base
   attr_accessible :city, :invitee_name, :state, :street_address, :suggested_end, :suggested_start, :user_id
   belongs_to :user
